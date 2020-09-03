1df80cadb071fb7c3707764f9d03ed50c32ffb93
Andrew R <30153408+HobbyTransform@users.noreply.github.com>
2020-09-03 18:47:56 +1000

    Update post.js

--- static/js/post.js
+++ static/js/post.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:3000/api/posts/";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = "https://node-expressjs-blogging-app.herokuapp.com/api/posts";
+const API_BASE_URL = "https://node-expressjs-blogging-app.herokuapp.com/";
 
 window.onload = () => {
     getPost();
